8bab66fd03bc3b70e7df726d64555a24705a3436
12tp12 <tomer.patel@gmail.com>
2017-03-18 17:16:45 +0200

    add FAB button

--- TodoListManager/app/src/main/java/com/example/todolistmanager/MainActivity.java
+++ TodoListManager/app/src/main/java/com/example/todolistmanager/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.todolistmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import 	android.app.AlertDialog;
+import 	android.widget.EditText;
+import android.util.Log;
+import android.content.DialogInterface;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +16,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_Button);
+
+        final EditText taskEditText;
+        AlertDialog dialog;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText taskEditText = new EditText(MainActivity.this);
+                AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Add a new task")
+                        .setMessage("What do I have to do now?")
+                        .setView(taskEditText)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String task = String.valueOf(taskEditText.getText());
+                                //Log.d(TAG, "Task to add: " + task);
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .create();
+                dialog.show();
+            }
+        });
     }
 
 

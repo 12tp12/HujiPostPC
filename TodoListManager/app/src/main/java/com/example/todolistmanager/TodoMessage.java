75aba8c2cff0abb7acef7594b7403681f08be502
12tp12 <tomer.patel@gmail.com>
2017-03-18 17:54:59 +0200

    Add TodoMessage class - represents a one todo msg

---
+++ TodoListManager/app/src/main/java/com/example/todolistmanager/TodoMessage.java
@@ -0,0 +1,53 @@
+package com.example.todolistmanager;
+
+/**
+ * Created by Tomer Patel on 3/18/2017.
+ */
+
+import android.util.Log;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+
+public class TodoMessage {
+
+    private String data;
+    private String hourCreated;
+    private String todoHour;
+
+    public TodoMessage(String data, String todoHour){
+        this.data = data;
+        this.todoHour = todoHour;
+        this.hourCreated = new SimpleDateFormat("HH:mm").format(new Date());
+        Log.i("Message info", "Date is " + this.hourCreated);
+
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+    public String getHourCreated() {
+        return hourCreated;
+    }
+
+    public void setHourCreated(String hourCreated) {
+        this.hourCreated = hourCreated;
+    }
+
+    public String getTodoHour() {
+        return todoHour;
+    }
+
+    public void setTodoHour(String todoHour) {
+        this.todoHour = todoHour;
+    }
+
+
+}
+
